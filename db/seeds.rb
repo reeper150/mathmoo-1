683c4f108453cd57898aee5f5f20be7a74174c11
zaguios <zaguios@gmail.com>
2013-05-15 23:49:42 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,9 @@ movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
 movies.each do |movie|
   Movie.create!(movie)
 end
+
+Game.create(id: 1, created_at: "2013-05-15 23:53:49", updated_at: "2013-05-15 23:53:49", swf: nil, swf_file_name: "badpiggies.swf", name: "Bad Piggies", description: "Help the mean pigs from Angry Birds collect all of ...", category: "Other", age_group: "K-3", dimension_x: 960, dimension_y: 600, image: nil, feature_image: nil, developer: "Rovio Entertainment Ltd", instructions: "Use the mouse to select the pigs and device parts.", date_to_go_live: "2013-05-15", date_to_show_countdown: "2013-05-15", plays: 0, image_file_name: "thumbnail.png", feature_image_file_name: nil)
+
+Game.create(id: 2, created_at: "2013-05-15 23:57:12", updated_at: "2013-05-15 23:57:12", swf: nil, swf_file_name: "575636_Penguin_OverlordsT2.swf", name: "Penguin Overlords", description: "Fight the evil turtle empire", category: "Skill", age_group: "3-6", dimension_x: 700, dimension_y: 540, image: nil, feature_image: nil, developer: "Christopher Gregorio", instructions: "Use your mouse", date_to_go_live: "2013-05-15", date_to_show_countdown: "2013-05-15", plays: 0, image_file_name: "icon.png", feature_image_file_name: "featureimage.png")
+
+Frontpage.create(id: 2, created_at: "2013-05-15 23:57:23", updated_at: "2013-05-15 23:57:23", featured_game: 2)
