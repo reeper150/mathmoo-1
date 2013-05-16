8443e65ed44380e2f5d325af336aaaa6ceca12da
zaguios <zaguios@gmail.com>
2013-05-15 23:32:10 -0700

    lesson routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   match 'movies/:id/similar' => 'movies#similar'
   root :to => redirect('/movies')
+
+  match '/add_lesson', :to => 'lessons#add_lesson'
+  match '/lessons/:id', :to => 'lessons#show', :format => false
+  resources :lessons do
+    member do
+      post 'upload'
+    end
+  end
 end
