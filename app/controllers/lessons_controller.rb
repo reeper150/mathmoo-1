9c53800c0a17e004654cd718ee77f4dfa857ae8c
zaguios <zaguios@gmail.com>
2013-05-15 22:46:42 -0700

    try try again

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,21 @@
+class LessonsController < ApplicationController
+  def add_lesson
+  end
+  def show
+    #@on_page = 2;
+    #@lesson = Lesson.find(params[:id])
+  end
+  def create
+    #@on_page = 0;
+    #@lesson = Lesson.new(params[:lesson])
+    #respond_to do |format|
+    #  if @lesson.save
+    #    format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
+    #    format.json { render json: @lesson, status: :created, location: @lesson }
+    #  else
+    #    format.html { render action: "new" }
+    #    format.json { render json: @lesson.errors, status: :unprocessable_entity }
+    #  end
+    #end
+  end
+end
---
+++ app/models/lesson.rb
@@ -0,0 +1,29 @@
+class Lesson < ActiveRecord::Base
+#attr_accessible  :name, :display_name, :description, :grade_level, :lesson_type, :subject, :views, :content, :content_file_name, :content2, :content2_file_name, :content3, :content3_file_name, :content4, :content4_file_name, :content5, :content5_file_name 
+#has_attached_file :content,
+#  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+#
+#has_attached_file :content2,
+#  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+#
+#has_attached_file :content3,
+#  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+#
+#has_attached_file :content4,
+#  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+#
+#has_attached_file :content5,
+#  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+#
+#def swfupload_file=(data)
+#    data.content_type =
+#MIME::Types.type_for(data.original_filename).first.content_type
+#    logger.warn("Data content type is: #{data.content_type}")
+#    self.file = data
+#  end
+end
